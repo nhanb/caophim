226307e0c45341db84977e596614988bf1b3dcb0
Bùi Thành Nhân <hi@imnhan.com>
2021-09-18 19:52:24 +0700

    migrate to debian 11

--- src/imgprocessing.nim
+++ src/imgprocessing.nim
@@ -4,7 +4,7 @@ import os, strformat
 proc createThumbnail*(picsDir: string, filename: string, format: string): string =
   let inFile = picsDir / filename & "." & format
   let outFile = picsDir / filename & ".thumb." & format
-  let thumbCmd = fmt"magick convert {inFile} -coalesce -resize 150x150\> {outFile}"
+  let thumbCmd = fmt"convert {inFile} -coalesce -resize 150x150\> {outFile}"
   echo thumbCmd
   let errC = execShellCmd(thumbCmd)
   echo "Create thumbnail status: " & $errC
